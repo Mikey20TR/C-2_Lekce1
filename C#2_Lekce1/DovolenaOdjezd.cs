763f84f0c91d1e7e5b0977c2c018910d6d913d9e
Mikey20TR <Mikejbc@email.cz>
2024-04-21 21:09:56 +0200

    Add project files.

---
+++ C#2_Lekce1/DovolenaOdjezd.cs
@@ -0,0 +1,25 @@
+﻿namespace C_2_Lekce1
+{
+    public class DovolenaOdjezd
+    {
+        public static void Main()
+        {
+            // Zbyvajici dny do dovolene
+            // Postupne se uzivatele zeptej na den, mesic a rok odjezdu na dovolenou. Pomoci DateTime a TimeSpan vypocitej pocet
+            // zbyvajicich dni do odjezdu na dovolenou a vypis je na konzoli.
+            // Pozn.: pro zjednoduseni staci pouzit int.Parse
+            Console.WriteLine("Zadej datum tvého odjezdu na dovolenou");
+            Console.WriteLine("Zadej den:");
+            int den = int.Parse(Console.ReadLine());
+            Console.WriteLine("Zadej měsíc:");
+            int mesic = int.Parse(Console.ReadLine());
+            Console.WriteLine("Zadej rok:");
+            int rok = int.Parse(Console.ReadLine());
+
+            DateTime Ted = DateTime.Today;
+            DateTime denOdjezdu = new DateTime(rok, mesic, den);
+            TimeSpan pocetDniDoOdjezdu = denOdjezdu - Ted;
+            Console.WriteLine($"Do odjezdu zbývá {pocetDniDoOdjezdu.TotalDays} dní, což je {pocetDniDoOdjezdu.TotalHours} hodin");
+        }
+    }
+}
---
+++ Lekce1_BreakOut2/ClovekVyjimky.cs
@@ -0,0 +1,66 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Numerics;
+
+namespace Lekce1_BreakOut2
+{
+    public class Clovek
+    {
+       public string Jmeno;
+        public int TelCislo;
+        public Clovek(string jmeno, int telCislo)
+        {
+            Jmeno = jmeno;
+            TelCislo = telCislo;
+            while (telCislo.ToString().Length < 9)
+            {
+                Console.WriteLine($"{telCislo} je neplatné telefonní číslo, musí obsahovat alespoň 9 číslic");
+                telCislo = int.Parse(Console.ReadLine());
+            }
+        }
+        public void VypisJmenoACislo()
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Jméno: {Jmeno}, Tel. číslo: {TelCislo}");
+            return;
+        }
+    }
+    public class ClovekVyjimky
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Program na vytvareni telefonniho seznamu");
+            Console.WriteLine("========================================");
+            Console.Write("Zadejte jmeno: ");
+            string jmeno = Console.ReadLine();
+            Console.Write("Zadejte telefonni cislo: ");
+            int cislo = 0;
+            try 
+            {
+                cislo = int.Parse(Console.ReadLine());
+            }
+            catch (FormatException vyjimka)
+            {
+                Console.WriteLine(vyjimka.Message);
+                Console.WriteLine("Nezadal jsi správně telefonní číslo");
+                return;
+            }
+            catch (OverflowException vyjimka)
+            {
+                Console.WriteLine(vyjimka.Message);
+                Console.WriteLine("Zadané číslo bylo moc velké");
+                return;
+            }
+            Clovek prvniClovek;
+            try
+            {
+                prvniClovek = new Clovek(jmeno, cislo);
+                prvniClovek.VypisJmenoACislo();
+            }
+            catch (Exception vyjimka)
+            {
+                Console.WriteLine(vyjimka.Message);
+            }
+            Console.WriteLine("=== Program končí ===");
+        }
+    }
+}
